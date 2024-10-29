4e047f644195aec81adf4388470a5184134e916c
PhantomStryfe <john.w.blackburnIV@gmail.com>
2024-10-29 15:06:26 -0400

    Test
    Test

---
+++ js/filter-conditionsdiseases.js
@@ -0,0 +1,57 @@
+"use strict";
+
+class PageFilterConditionsDiseases extends PageFilterBase {
+	// region static
+	static getDisplayProp (prop) {
+		return prop === "status" ? "Other" : Parser.getPropDisplayName(prop);
+	}
+	// endregion
+
+	constructor () {
+		super();
+
+		this._typeFilter = new Filter({
+			header: "Type",
+			items: ["condition", "disease", "status"],
+			displayFn: PageFilterConditionsDiseases.getDisplayProp,
+			deselFn: (it) => it === "disease" || it === "status",
+		});
+		this._miscFilter = new Filter({
+			header: "Miscellaneous",
+			items: ["Legacy", "Has Images", "Has Info"],
+			isMiscFilter: true,
+			deselFn: PageFilterBase.defaultMiscellaneousDeselFn.bind(PageFilterBase),
+		});
+	}
+
+	static mutateForFilters (it) {
+		this._mutateForFilters_commonSources(it);
+		this._mutateForFilters_commonMisc(it);
+	}
+
+	addToFilters (it, isExcluded) {
+		if (isExcluded) return;
+
+		this._sourceFilter.addItem(it._fSources);
+		this._miscFilter.addItem(it._fMisc);
+	}
+
+	async _pPopulateBoxOptions (opts) {
+		opts.filters = [
+			this._sourceFilter,
+			this._typeFilter,
+			this._miscFilter,
+		];
+	}
+
+	toDisplay (values, it) {
+		return this._filterBox.toDisplay(
+			values,
+			it._fSources,
+			it.__prop,
+			it._fMisc,
+		);
+	}
+}
+
+globalThis.PageFilterConditionsDiseases = PageFilterConditionsDiseases;
